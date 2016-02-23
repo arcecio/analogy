d72bac466844a62c257b7a1afe7707f8c1de4d30
Angel Darquea <angeldarquea@gmail.com>
2016-02-22 22:08:21 -0800

    Initial commit

---
+++ src/main/java/com/analogy/AnalogyApplication.java
@@ -0,0 +1,12 @@
+package com.analogy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalogyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalogyApplication.class, args);
+	}
+}
